2ca66da280179bf76148514c67f978a8156f463a
sai kumar ekula <sai.ekula.ext@siemens.com>
2017-12-19 14:20:28 -0500

    Interpolation, Binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number = 4;
+  btnText: string = 'Add an item';
+  itemText: string = 'My first list item';
+  items = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.items.length;
+  }
+
+  addItem() {
+    this.items.push(this.itemText);
+    this.itemText = '';
+    this.itemCount = this.items.length;
   }
 
 }
