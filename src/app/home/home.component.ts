8154b33b807de159355ffccc95b6d0ea29b3011b
sai kumar ekula <sai.ekula.ext@siemens.com>
2017-12-19 15:39:01 -0500

    Animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+
+    trigger('items', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0}), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+          ]))
+        ]), {optional: true}),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+          ]))
+        ]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
   itemCount: number = 4;
   btnText: string = 'Add an item';
   itemText: string = 'My first list item';
-  items = [];
+  items = ['Gas turbine', 'Power Plant', 'Units'];
 
   constructor() { }
 
@@ -22,6 +47,10 @@ export class HomeComponent implements OnInit {
     this.items.push(this.itemText);
     this.itemText = '';
     this.itemCount = this.items.length;
+  }
+
+  removeItem(i) {
+    this.items.splice(i, 1);
   }
 
 }
