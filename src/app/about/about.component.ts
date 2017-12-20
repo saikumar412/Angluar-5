22eb7a89af6acf423e55ed2e8b2b4d1f872bf971
saikumar412 <ekula.saikumar@gmail.com>
2017-12-20 13:46:53 -0500

    Data service import

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-about',
@@ -9,11 +10,14 @@ import { Router } from '@angular/router';
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private router: Router) {
+  items: any;
+
+  constructor(private route: ActivatedRoute, private router: Router, private _data: DataService) {
     this.route.params.subscribe(res => console.log(res.id));
    }
 
   ngOnInit() {
+    this._data.item.subscribe(res => this.items = res);
   }
 
   sendMeHome() {
@@ -21,3 +25,4 @@ export class AboutComponent implements OnInit {
   }
 
 }
+
