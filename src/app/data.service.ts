d314025664fb27c804f82b42342805f5151fabea
sai kumar ekula <sai.ekula.ext@siemens.com>
2017-12-20 13:48:17 -0500

    service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class DataService {
 
+  private items = new BehaviorSubject<any>(['The initiall item', 'Added unit item']);
+  item = this.items.asObservable();
+
   constructor() { }
 
+  changeItem(item) {
+    this.items.next(item);
+  }
+  
 }
